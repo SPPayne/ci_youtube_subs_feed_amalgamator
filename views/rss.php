2f50279a640f08e4bed712ac1aa86ce39820d0ff
sppayne <sppayne@gmail.com>
2015-06-22 20:54:41 +0100

    RSS tweaks
    Escaping characters in titles, correct content type (UTF-8).

--- views/rss.php
+++ views/rss.php
@@ -1,5 +1,5 @@
-<?php header("Content-Type: application/rss+xml; charset=ISO-8859-1"); ?>
-<?php echo '<?xml version="1.0" encoding="ISO-8859-1"?>';//Echo otherwise it is interpreted as PHP! ?>
+<?php header("Content-Type: application/rss+xml; charset=UTF-8"); ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>';//Echo otherwise it is interpreted as PHP! ?>
 <rss version="2.0">
 	<channel>
 		<title><?php echo $rss_title; ?></title>
@@ -10,9 +10,10 @@
 		<?php if($rss_items){ //If items... ?>
 			<?php foreach($rss_items as $timestamp => $item){ ?>
 				<item>
-					<title><?php echo $item['title']; ?></title>
+					<title><![CDATA[<?php echo $item['title']; ?>]]></title>
 					<description><![CDATA[<?php echo $item['desc']; ?>]]></description>
 					<link><?php echo $item['link']; ?></link>
+					<guid><?php echo $item['link']; ?></guid>
 					<pubDate><?php echo date("D, d M Y H:i:s O", $timestamp); ?></pubDate>
 				</item>
 			<?php } ?>
